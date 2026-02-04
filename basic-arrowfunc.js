3d6a6975941c2a930a381abf099e46df04727251
Harsh-Tripathi-231103 <harsh2454@gmail.com>
2026-02-04 07:21:27 +0530

    Basic quetsions based on conditions in javascript

---
+++ basic-arrowfunc.js
@@ -0,0 +1,5 @@
+const func1 = (a, b) => {
+    return a + b;
+}
+console.log(func1(34,34))
+// addition of numbers using arrow functions
---
+++ sq_arroefunc.js
@@ -0,0 +1,5 @@
+const squ = (x) =>{
+    return x*x
+
+}
+console.log(squ(5))
