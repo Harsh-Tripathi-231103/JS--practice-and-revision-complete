a097915270bc65a9d0f74046d138269cb39e69da
Harsh-Tripathi-231103 <harsh2454@gmail.com>
2026-04-25 16:56:29 +0530

    playing with objects using direct, string and dynamic key concept

--- objectsandchange.js
+++ objectsandchange.js
@@ -6,6 +6,11 @@ const test_obj={
 
 test_obj.number=18 // chnaging/updating its value
 
+test_obj["name"]="harsh"
+
 console.log(test_obj) // new updated value prints out in console
 
 
+// obj.name = "harsh";      // direct key
+// obj["name"] = "harsh";   // string key
+// obj[key] = "harsh";      // dynamic key from variable
