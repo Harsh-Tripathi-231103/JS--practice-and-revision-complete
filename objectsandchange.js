dc028a6a62c2b982ba8fb02ced8459d21d4ecabc
Harsh-Tripathi-231103 <harsh2454@gmail.com>
2026-04-25 16:45:42 +0530

    create a const obj in js . also can you change it to hold a number later

---
+++ objectsandchange.js
@@ -0,0 +1,11 @@
+// create a const obj in js . also can you change it to hold a number later
+
+const test_obj={
+    number:10                      // creating a const obj 
+}
+
+test_obj.number=18 // chnaging/updating its value
+
+console.log(test_obj) // new updated value prints out in console
+
+
