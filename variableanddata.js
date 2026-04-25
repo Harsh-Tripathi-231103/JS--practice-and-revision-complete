6470665f03225b3c72a25680b82fd00e13e28902
Harsh-Tripathi-231103 <harsh2454@gmail.com>
2026-04-25 16:19:07 +0530

    program to store data using objects in js

---
+++ variableanddata.js
@@ -0,0 +1,9 @@
+// write a js program to store name, phone number and marks of a student using objects.
+
+const student_directory ={
+    name:"Harsh Tripathi",
+    phone_number: "8433532963",
+    marks:"100"
+}
+
+console.log(student_directory)
